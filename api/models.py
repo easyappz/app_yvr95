50432da2064239682b3b85dc429a69f63dee8ecc
IbragimKhaniev <newinterny@gmail.com>
2025-12-04 14:32:00 +0300

    Implement data models and serializers: Member (custom user), Post, Like, Comment with corresponding DRF serializers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,87 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, UserMixin
 from django.db import models
 
-# Create your models here.
+
+class MemberManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class Member(AbstractBaseUser, UserMixin, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    about_me = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = MemberManager()
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Member, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Post by {self.author.username} at {self.created_at}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f'{self.user.username} likes post #{self.post.id}'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on post #{self.post.id}'
--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,51 @@
 from rest_framework import serializers
+from .models import Member, Post, Like, Comment
 
 
-class MessageSerializer(serializers.Serializer):
-    message = serializers.CharField(max_length=200)
-    timestamp = serializers.DateTimeField(read_only=True)
+class MemberSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Member
+        fields = ('id', 'username', 'first_name', 'last_name', 'about_me', 'password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        if password is None:
+            raise serializers.ValidationError({"password": "This field is required."}) 
+        user = Member(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = MemberSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'content', 'created_at', 'updated_at')
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = MemberSerializer(read_only=True)
+    post = PostSerializer(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ('id', 'user', 'post', 'created_at')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = MemberSerializer(read_only=True)
+    post = PostSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'user', 'post', 'content', 'created_at')
