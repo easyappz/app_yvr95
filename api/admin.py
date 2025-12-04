a5e2f5b407a6c3ce4966cf6de797b1b5f4da7a87
IbragimKhaniev <newinterny@gmail.com>
2025-12-04 14:35:33 +0300

    feat: register models in admin and configure admin site titles

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Member, Post, Like, Comment
 
-# Register your models here.
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'first_name', 'last_name')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'created_at', 'updated_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('content', 'author__username')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
+    list_filter = ('user', 'post', 'created_at')
+    search_fields = ('user__username', 'post__content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at', 'content')
+    list_filter = ('user', 'post', 'created_at')
+    search_fields = ('user__username', 'post__content', 'content')
---
+++ config/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Set admin site titles
+admin.site.site_header = 'Social Network Administration'
+admin.site.site_title = 'Social Network Administration'
+admin.site.index_title = 'Welcome to Social Network Administration'
